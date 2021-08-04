233f7634e73410edf5e75d6d2f8e920c042fbcba
marqueswsm <wagner_marques@live.com>
2021-08-04 14:58:30 -0700

    finish controller of users

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,9 +1,81 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/marqueswsm/web-api-with-golang/database"
+	"github.com/marqueswsm/web-api-with-golang/models"
+)
 
 func ShowUser(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+	id := c.Param("id")
+	newid, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID must to be integer",
+		})
+
+		return
+	}
+
+	db := database.GetDatabase()
+
+	var user models.User
+	err = db.First(&user, newid).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "User not found: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, user)
+}
+
+func CreateUser(c *gin.Context) {
+	db := database.GetDatabase()
+	var user models.User
+
+	err := c.ShouldBindJSON(&user)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Failed to parse user to json",
+		})
+
+		return
+	}
+
+	err = db.Create(&user).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Failed to create an user",
+		})
+
+		return
+	}
+
+	c.JSON(200, user)
+}
+
+func ShowAllUsers(c *gin.Context) {
+	db := database.GetDatabase()
+	var users []models.User
+
+	err := db.Find(&users).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Failed return all users",
+		})
+
+		return
+	}
+
+	c.JSON(200, users)
 }
