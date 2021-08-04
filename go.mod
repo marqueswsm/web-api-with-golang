79bdca57e5e4ca57c9cb13bcdb78f9be02214d3b
marqueswsm <wagner_marques@live.com>
2021-08-04 14:08:01 -0700

    create model, migration and database

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/marqueswsm/web-api-with-golang
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.3 // indirect
+	gorm.io/driver/postgres v1.1.0 // indirect
+	gorm.io/gorm v1.21.12 // indirect
+)
---
+++ models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	Name      string         `json:"name"`
+	CreatedAt time.Time      `json:"created"`
+	UpdatedAt time.Time      `json:"updated"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted"`
+}
